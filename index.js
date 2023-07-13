19215f5efb88975219dde974e0c7811dd83c1d70
tajkir-alam <tajkir.alam@gmail.com>
2023-07-13 19:05:49 +0600

    menu db created

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const app = express()
 require('dotenv').config()
-const port = 3000
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2ziqne.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = `mongodb+srv://favfood:0DHUoq8vXFzqZuFx@cluster0.h2ziqne.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,28 +25,33 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
+const dbConnect = async () => {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
+        console.log("Database Connected Successfully with MongoDB");
 
-        const usersCollection = client.db("FavFood").collection("users");
-
-
-        app.get('/users', async (req, res) => {
-            const users = usersCollection.find()
-            const cursor = await users.toArray();
-            res.send(cursor);
-        })
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+    } catch (error) {
+        console.log(error.name, error.message);
     }
 }
-run().catch(console.dir);
+dbConnect()
+
+
+const usersCollection = client.db("FavFood").collection("users");
+const menuCollection = client.db("FavFood").collection("menu");
+
+
+app.get('/users', async (req, res) => {
+    const users = usersCollection.find()
+    const cursor = await users.toArray();
+    res.send(cursor);
+})
+
+app.get('/menu', async (req, res) => {
+    const menu = menuCollection.find();
+    const cursor = await menu.toArray();
+    res.send(cursor);
+})
 
 
 app.listen(port, () => {
